286cd6340f9c582a1945e463b399cc2bc025364b
mkds622 <mkds622@gmail.com>
2017-08-24 11:46:38 +0530

    Commit 3

--- Learn You Mongo/Find.js
+++ Learn You Mongo/Find.js
@@ -1,9 +1,9 @@
 var mongo = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/learnyoumongo";
 var ageCriteria = parseInt(process.argv[2]);
-mongo.connect(url, function(err,db){
+mongo.connect(url, function(err, db) {
     var parrots = db.collection('parrots');
-    parrots.find({'age': {'$gt': ageCriteria}}).toArray(function(err,docs){
+    parrots.find({ 'age': { '$gt': ageCriteria } }).toArray(function(err, docs) {
         //docs.forEach(doc => console.log(doc) );
         console.log(docs);
         db.close();
